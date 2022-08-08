c488ea5592245e5cd622a79ab85aac4670710a84
André Oliveira <andreosx@gmail.com>
2022-08-08 12:22:09 -0300

    Aplicando injeção de dependência.

---
+++ app/src/main/java/com/picpay/desafio/android/util/retrofit/Service.kt
@@ -0,0 +1,20 @@
+package com.picpay.desafio.android.util.retrofit
+
+import com.google.gson.GsonBuilder
+import com.picpay.desafio.android.util.Constants
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class RetrofitService {
+    
+    private val retrofit = Retrofit.Builder()
+        .baseUrl(Constants.url)
+        .client(OkHttpClient.Builder().build())
+        .addConverterFactory(GsonConverterFactory.create(GsonBuilder().create()))
+        .build()
+
+    fun <API> createService(apiClass: Class<API>): API {
+        return retrofit.create(apiClass)
+    }
+}
